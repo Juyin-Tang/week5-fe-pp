f0fb563aafdd8729a0e8fd2bfb738747746e8eec
atinhutlk <nhut.vo@metropolia.fi>
2025-11-24 21:25:50 +0200

    [iter4] Display individual job details

--- frontend/src/pages/JobPage.jsx
+++ frontend/src/pages/JobPage.jsx
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
 
-
-const apiUrl = 'http://localhost:4000/api/jobs';
-
-
+const apiUrl = "/api/jobs";
 
 const JobPage = () => {
   const { id } = useParams();
@@ -15,6 +12,13 @@ const JobPage = () => {
     const fetchJobs = async () => {
       try {
         const response = await fetch(`${apiUrl}/${id}`);
+
+        
+        if (!response || !response.ok) {
+          console.error("Failed to fetch job");
+          return;
+        }
+
         const data = await response.json();
         console.log("Job by ID:", data);
         setJob(data);
@@ -22,46 +26,47 @@ const JobPage = () => {
         console.error("Failed to fetch", err);
       }
     };
-    fetchJobs();
-  });
 
-  
+    fetchJobs();
+  }, [id]); 
+
   const deleteJob = async () => {
     try {
-      const response = await fetch(`${apiUrl}/${id}`,{
-        method:"DELETE",
+      const response = await fetch(`${apiUrl}/${id}`, {
+        method: "DELETE",
       });
-      
+
       if (response.ok) {
         console.log("Deleting successfully");
         navigate("/");
       } else {
         console.error("Failed to delete job");
       }
-
     } catch (error) {
       console.error("Error deleteing job:", error);
     }
   };
+
   if (!job) {
     return <div>Loading...</div>;
   }
-
 
   return (
     <div className="job-details">
       <h2>{job.title}</h2>
       <p>Type: {job.type}</p>
       <p>Description: {job.description}</p>
-      <p>Company: {job.company.name}</p>
-      <p>Contact Email: {job.company.contactEmail}</p>
-      <p>Contact Phone: {job.company.contactPhone}</p>
+      <p>Company: {job.company?.name}</p>
+      <p>Contact Email: {job.company?.contactEmail}</p>
+      <p>Contact Phone: {job.company?.contactPhone}</p>
       <p>Location: {job.location}</p>
       <p>Salary: {job.salary}</p>
       <p>Posted Date: {job.postedDate}</p>
+
       <Link to={`/edit-job/${id}`}>
         <button>Edit Job</button>
       </Link>
+
       <button onClick={deleteJob}>Delete Job</button>
     </div>
   );
